7e74321ba950c1a7c428f4a1638cf47be939286e
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-03-24 15:41:18 +0100

    Initial commit

---
+++ app/src/main/java/eu/szkolny/sslprovider/MainActivity.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Kuba Szczodrzyński 2021-3-24.
+ */
+
+package eu.szkolny.sslprovider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
